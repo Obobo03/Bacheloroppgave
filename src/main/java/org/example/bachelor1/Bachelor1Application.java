6ffd8fbe65faa1355a5fb0570fde68df201b5925
oweno <owenogbebo@gmail.com>
2025-02-17 10:07:43 +0100

    Første commit :)

---
+++ src/main/java/org/example/bachelor1/Bachelor1Application.java
@@ -0,0 +1,13 @@
+package org.example.bachelor1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bachelor1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bachelor1Application.class, args);
+    }
+
+}
